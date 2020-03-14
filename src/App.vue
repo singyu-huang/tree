a25b43175773ff16303967719b694d65dc047b83
singyu-huang <hanigogoph@gmail.com>
2020-03-14 17:44:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <GoogleMap msg="123" />
+    </div>
+</template>
+<script>
+import GoogleMap from './components/GoogleMap.vue'
+
+export default {
+    name: 'App',
+    components: {
+        GoogleMap
+    }
+}
+</script>
+<style>
+#app {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+</style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="google-map" id="map"></div>
+</template>
+<script>
+export default {
+    name: 'GoogleMap',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            map: null
+        };
+    },
+    mounted() {
+        this.initMap();
+    },
+    methods: {
+        initMap() {
+            const google = window.google
+            this.map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8
+            });
+        }
+    }
+}
+</script>
+<style scoped>
+#map {
+    height: 100%;
+}
+
+html,
+body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+</style>
